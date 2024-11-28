83014207e713fe81490a0881f70ea2f4d31a2c9e
Awsaf Fida Mahmud <fidaawsaf@gmail.com>
2024-11-28 11:17:20 -0500

    Moved router component to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Link} from "react-router-dom";
+import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
+import Home from "./Screens/Home/Home";
+import Login from "./Screens/Auth/Login";
+import Signup from "./Screens/Auth/Signup";
+import NoPage from "./Screens/_NotFound/NoPage";
 
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Routes>
+            <Route index element={<Home/>}/>
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="*" element={<NoPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -13,16 +13,9 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-      <BrowserRouter>
-          <Routes>
-              <Route path="/" element={<App />}>
-                  <Route index element={<Home/>}/>
-                  <Route path="/login" element={<Login />} />
-                  <Route path="/signup" element={<Signup />} />
-                  <Route path="*" element={<NoPage />} />
-              </Route>
-          </Routes>
-      </BrowserRouter>
+      <React.StrictMode>
+          <App/>
+      </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
