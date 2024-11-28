00d467fb7e76f46b0cd81b7bb6bd27a04c3c5756
Awsaf Fida Mahmud <fidaawsaf@gmail.com>
2024-11-28 10:49:13 -0500

    set up react router dom navigation

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Login from "./Screens/Auth/Login";
+import NoPage from "./Screens/_NotFound/NoPage";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+      <BrowserRouter>
+          <Routes>
+              <Route path="/" element={<App />}>
+                  <Route path="login" element={<Login />} />
+                  {/*<Route path="signup" element={<Signup />} />*/}
+                  <Route path="*" element={<NoPage />} />
+              </Route>
+          </Routes>
+      </BrowserRouter>
   </React.StrictMode>
 );
 
