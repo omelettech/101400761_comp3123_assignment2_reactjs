18620149b38911f4452691a6f86c8d0224440289
Awsaf Fida Mahmud <fidaawsaf@gmail.com>
2024-11-28 11:12:58 -0500

    Added home page

---
+++ src/Screens/Home/Home.tsx
@@ -0,0 +1,12 @@
+import {Link} from "react-router-dom";
+import React from "react";
+
+
+export default function Home(){
+    return(
+        <div>
+            <div>asdasd</div>
+            <Link to={"/login"}>Login</Link>
+        </div>
+    )
+}
