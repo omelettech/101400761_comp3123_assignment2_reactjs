b0309442cc31b3e39855dc646e65f1c5e21575a9
Awsaf Fida Mahmud <fidaawsaf@gmail.com>
2024-11-28 11:19:27 -0500

    Updated forms component styling

--- src/Screens/Auth/_Form.tsx
+++ src/Screens/Auth/_Form.tsx
@@ -2,25 +2,33 @@ import React, {SetStateAction} from "react";
 
 
 interface AuthFormProps{
+    label:string
     username:string
     setUsername:React.Dispatch<React.SetStateAction<string>>
     password:string
     setPassword:React.Dispatch<React.SetStateAction<string>>
 
 }
-export default function AuthForm({username,password,setUsername,setPassword}:AuthFormProps){
+export default function AuthForm({label,username,password,setUsername,setPassword}:AuthFormProps){
     return(
         <div style={{
             display:"flex",
+            flexDirection:"column",
             alignItems:"center",
+            width:"fit-content",
+            padding:20,
+
             justifyContent:"center",
             borderRadius:10,
             backgroundColor:"#eee8e8"
         }}>
+            <h2>{label}</h2>
+            <p>Username</p>
             <input
                 value={username}
                 onChange={(val)=>setUsername(val.target.value)}
             />
+            <p>Password</p>
             <input
                 type={"password"}
                 value={password}
